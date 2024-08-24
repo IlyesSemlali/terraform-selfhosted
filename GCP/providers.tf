cbf6e837b2d99729b49cee99ec157530a6b18f46
Ilyes Semlali <ilyes.sem@gmail.com>
2024-08-24 20:55:16 +0200

    fix(terraform): add github to required providers

--- GCP/providers.tf
+++ GCP/providers.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "fluxcd/flux"
       version = "1.3.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.4"
