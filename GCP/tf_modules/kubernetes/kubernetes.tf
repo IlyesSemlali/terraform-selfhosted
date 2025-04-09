0dfdc20935f5ad1b50b7b3a777fbd07f9e5f71a6
Ilyes Semlali <ilyes.sem@gmail.com>
2025-04-09 17:34:21 +0200

    feat(network): taint nodes to allow Cilium installation

--- GCP/tf_modules/kubernetes/kubernetes.tf
+++ GCP/tf_modules/kubernetes/kubernetes.tf
@@ -23,6 +23,13 @@ resource "google_container_node_pool" "permanent" {
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.gke_sa.email
+
+    taint {
+      key    = "node.cilium.io/agent-not-ready"
+      value  = "true"
+      effect = "NO_EXECUTE"
+    }
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
@@ -48,6 +55,13 @@ resource "google_container_node_pool" "extra" {
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.gke_sa.email
+
+    taint {
+      key    = "node.cilium.io/agent-not-ready"
+      value  = "true"
+      effect = "NO_EXECUTE"
+    }
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
