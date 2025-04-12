38d697879a3dece24e698cdfa6e721b1bbeabace
Ilyes Semlali <ilyes.sem@gmail.com>
2025-04-12 16:32:05 +0200

    fix(authentik): wait for API to be ready

--- GCP/tf_modules/kubernetes_provisionning/authentik.tf
+++ GCP/tf_modules/kubernetes_provisionning/authentik.tf
@@ -52,20 +52,53 @@ resource "kubernetes_secret" "authentik_values" {
 }
 
 ####################################
+## Wait for authentik to be ready ##
+####################################
+
+data "http" "authentik_health" {
+  url = "https://auth.bobr.cloud/api/v3/core/system/health/"
+  request_headers = {
+    Authorization = "Bearer ${var.bootstrap_token}"
+    Accept        = "application/json"
+  }
+
+  # Wait up to 5 minutes for the API to respond with 200
+  retry {
+    attempts     = 20
+    min_delay_ms = 5000
+  }
+}
+
+resource "null_resource" "wait_for_authentik" {
+  triggers = {
+    health_status = data.http.authentik_health.status_code
+  }
+}
+
+####################################
 ## Common Authentik Configuration ##
 ####################################
 
+# TODO: import authentik integration to avoid duplicate:
+# https://developer.hashicorp.com/terraform/language/import
+
 data "authentik_flow" "default_invalidation_flow" {
   slug = "default-provider-invalidation-flow"
+
+  depends_on = [null_resource.wait_for_authentik]
 }
 
 data "authentik_flow" "default_authorization_flow" {
   slug = "default-provider-authorization-implicit-consent"
+
+  depends_on = [null_resource.wait_for_authentik]
 }
 
 resource "authentik_service_connection_kubernetes" "local" {
   name  = "Kubernetes"
   local = true
+
+  depends_on = [null_resource.wait_for_authentik]
 }
 
 resource "authentik_outpost" "forward_auth_outpost" {
