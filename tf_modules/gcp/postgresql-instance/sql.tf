6ce52952b5dd30851bf7cb65d26a16ab3bdbae1f
Ilyes Semlali <ilyes.sem@gmail.com>
2025-05-13 23:41:35 +0200

    fix(cloudsql): fix the deletion issue
    Here's the TF error that was thrown when destroying the peering:
    
    Unable to remove Service Networking Connection, err: Error waiting for Delete Service Networking Connection: Error code 9, message: Failed to delete connection; Producer services (e.g. CloudSQL, Cloud Memstore, etc.) are still using this connection.

--- tf_modules/gcp/postgresql-instance/sql.tf
+++ tf_modules/gcp/postgresql-instance/sql.tf
@@ -14,6 +14,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = data.google_compute_network.primary_network.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  deletion_policy         = "ABANDON"
 }
 
 resource "google_sql_database_instance" "postgresql" {
