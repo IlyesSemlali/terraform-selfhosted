c7dcf4d0d4748a4ca6f5ddd31ef0d92950498254
Ilyes Semlali <ilyes.sem@gmail.com>
2025-05-14 19:55:13 +0200

    chore(tf): remove unused variables, set types
    Now is the time to clean up the code, and make it more
    maintainable. A few more commits are comming

--- 03_applications/variables.tf
+++ 03_applications/variables.tf
@@ -1,41 +1,52 @@
 variable "project" {
   description = "Project name"
+  type        = string
 }
 
 variable "domain" {
   description = "DNS Domain "
+  type        = string
 }
+
 
 variable "owner_email" {
   description = "Platform owner's email address"
+  type        = string
   # TODO: add validation
 }
 
 variable "region" {
   description = "Belgium"
+  type        = string
   default     = "europe-west1"
 }
 
 variable "zone" {
   description = "Google Cloud default compute zone"
+  type        = string
   default     = "europe-west1-d"
 }
 
 variable "flux_repository" {
   description = "Gitrepository where Flux will initialize and get its sources"
+  type        = string
 }
 
 variable "github_owner" {
   description = "Github user or organization owning the flux repositories"
+  type        = string
 }
 
+# TODO: Generate this if not needed
 variable "authentik_bootstrap_password" {
   description = "Authentik akadmin password"
+  type        = string
   sensitive   = true
 }
 
+# TODO: Generate this if not needed
 variable "authentik_bootstrap_token" {
   description = "Authentik akadmin token"
+  type        = string
   sensitive   = true
 }
-
