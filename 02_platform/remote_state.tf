a92e38a809c7723e48cd08a0b8b3001b214638ef
Ilyes Semlali <ilyes.sem@gmail.com>
2025-05-14 19:55:13 +0200

    chore(tf): remove unused variables, set types
    Now is the time to clean up the code, and make it more
    maintainable. A few more commits are comming

--- 02_platform/remote_state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "foundation" {
-  backend = "local"
-
-  config = {
-    path = "../01_foundation/terraform.tfstate"
-  }
-}
-
--- 02_platform/variables.tf
+++ 02_platform/variables.tf
@@ -1,68 +1,59 @@
 variable "project" {
   description = "Project name"
-}
-
-variable "domain" {
-  description = "DNS Domain "
-}
-
-variable "owner_email" {
-  description = "Platform owner's email address"
-  # TODO: add validation
+  type        = string
 }
 
 variable "region" {
   description = "Belgium"
+  type        = string
   default     = "europe-west1"
 }
 
 variable "zone" {
   description = "Google Cloud default compute zone"
+  type        = string
   default     = "europe-west1-d"
 }
 
 variable "deletion_protection" {
   description = "Whether to block cluster deletion"
+  type        = bool
   default     = true
 }
 
 variable "kubernetes_cluster_name" {
   description = "Kubernetes cluster name"
+  type        = string
   default     = "self-hosted"
 }
 
 variable "kubernetes_permanent_nodes_type" {
   description = "Instances types for the Kubernetes cluster nodes, can be pricy !"
+  type        = string
   default     = "e2-standard-2"
 }
 
-variable "kubernetes_permanent_nodes_count" {
-  description = "Number of instances for the Kubernetes permanent node pool"
-  default     = 1
-}
+# variable "kubernetes_permanent_nodes_count" {
+#   description = "Number of instances for the Kubernetes permanent node pool"
+#   type        = number
+#   default     = 1
+# }
 
 variable "kubernetes_extra_nodes_type" {
   description = "Instances types for the Kubernetes cluster nodes, can be pricy !"
+  type        = string
   default     = "e2-standard-2"
 }
 
 variable "kubernetes_min_extra_nodes" {
   description = "Minimum amount of nodes on the cluster"
+  type        = number
   default     = 1
 }
 
 variable "kubernetes_max_extra_nodes" {
   description = "Maximum amount of nodes on the cluster"
+  type        = number
   default     = 1
-}
-
-variable "authentik_bootstrap_password" {
-  description = "Authentik akadmin password"
-  sensitive   = true
-}
-
-variable "authentik_bootstrap_token" {
-  description = "Authentik akadmin token"
-  sensitive   = true
 }
 
