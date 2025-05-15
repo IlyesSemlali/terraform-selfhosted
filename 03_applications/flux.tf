5ddda79fdd4ae3a723086bf1cd3fcd59e6914fc8
Ilyes Semlali <ilyes.sem@gmail.com>
2025-05-16 01:14:01 +0200

    fix(flux): wait for storage before bootstraping

--- 03_applications/flux.tf
+++ 03_applications/flux.tf
@@ -2,4 +2,6 @@ module "flux" {
   source            = "../tf_modules/agnostic/flux/"
   github_owner      = var.github_owner
   github_repository = var.flux_repository
+
+  depends_on = [module.storage]
 }
